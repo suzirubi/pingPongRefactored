9867b7af76fbb7865dc06667d3b18a06c37fdf22
Suzi Rubino <suzi@suzirubi.com>
2016-10-28 00:29:16 -0700

    separate business logic from user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,55 @@
 
+// business logic
+
+function falsey1(input) {
+  var falseString = input.toString();
+    if (falseString === "NaN") {
+      return true;
+    }
+}
+
+function falsey2(input) {
+  var falseString = input.toString();
+    if (falseString.match(/^[0-9]+$/) === null) {
+      return true;
+  }
+}
+
+function falsey3(input) {
+  var inputInt = parseFloat(input);
+  if ((inputInt % 1 != 0)|| inputInt <=1) {
+    return true;
+  }
+}
 
 
 // begin user interface
-$(document).ready(function(){
-  $("form#pingPongForm").submit(function(event) {
+$(document).ready(function() {
 
-    var userNumber = parseFloat($("input#userInput").val());
-    var falsey = String(userNumber);
+  $("#pingPongForm").submit(function(event) {
+    event.preventDefault();
 
+    var input = $("#userInput").val();
+    var false1 = falsey1(input);
+    var false2 = falsey2(input);
+    var false3 = falsey3(input);
 
-
-
-
-      if (falsey === "NaN") {
-        $(".falsey").toggle();
-        $(".falsey").text("Enter numbers silly!");
-      } else if ((userNumber % 1 != 0)|| userNumber <=1) {
-        $(".falsey").toggle();
-        $(".falsey").text("Ahh... now you're making it too hard, enter a whole number greater than 1!");
-      } else if (falsey.match(/^[0-9]+$/) === null) {
-        $(".falsey").toggle();
-        $(".falsey").text("JUST numbers greater than 1");
-      }
-
+    if (false1 = true) {
+      $(".falsey").toggle();
+      $(".falsey").text("Enter numbers silly!");
+    } else if (false2 = true) {
+      $(".falsey").toggle();
+      $(".falsey").text("Ahh... now you're making it too hard, enter a whole number greater than 1!");
+    } else if (false3 = true) {
+      $(".falsey").toggle();
+      $(".falsey").text("JUST numbers greater than 1");
+    }
       // groceryList.forEach(function(grocery){
       //     $("ul").append("<li>" + grocery + "</li>");
       // });
 
       // $("#display").text(result);
 
-    event.preventDefault();
   });
 
 });
