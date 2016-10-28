66679b57ada7d7caf175c42904749db8bac11724
Suzi <suzi@suzirubi.com>
2016-10-27 23:02:21 -0700

    finish falsey statements

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,33 @@
 
 // begin user interface
 $(document).ready(function(){
-  $("form#pingPongForm").submit(function(event){
+  $("form#pingPongForm").submit(function(event) {
 
-    var userInput = parseFloat($("input#userInput").val());
-    // var finalOutput = sequence(input);
-      $("#sequence").show();
-      $("#sequence").text((if ("userInput" === NaN){
-        return "Enter a number greater than 1 silly!";
-        }
-      ));
+    var userNumber = parseFloat($("input#userInput").val());
+    var falsey = String(userNumber);
 
 
 
+
+
+      if (falsey === "NaN") {
+        $(".falsey").toggle();
+        $(".falsey").text("Enter numbers silly!");
+      } else if ((userNumber % 1 != 0)|| userNumber <=1) {
+        $(".falsey").toggle();
+        $(".falsey").text("Ahh... now you're making it too hard, enter a whole number greater than 1!");
+      } else if (falsey.match(/^[0-9]+$/) === null) {
+        $(".falsey").toggle();
+        $(".falsey").text("JUST numbers greater than 1");
+      }
+
+      // groceryList.forEach(function(grocery){
+      //     $("ul").append("<li>" + grocery + "</li>");
+      // });
+
+      // $("#display").text(result);
 
     event.preventDefault();
   });
+
 });
