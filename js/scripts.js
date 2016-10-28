e28230fa42510e3ff8d9587816eaa4bf6ddad047
Suzi <Guest@epicodus-21.local>
2016-10-28 11:13:36 -0700

    fixed functionality of ul element to show and hide as user enters new numbers

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,12 @@ function falsey2(input) {
 }
 
 function sequenceFinal(input) {
-  var numberSequence = 
-  var sequenceArray = input.split(",");
-
-  console.log(sequenceArray);
+  var length = input.toString();
+  console.log(length);
+  var sequenceArray = [];
+  for ( var number = 1; number < length; number++ ) {
+    sequenceArray.push(number);
+  }
   var output = sequenceArray;
   return output;
 }
@@ -32,19 +34,17 @@ $(document).ready(function() {
     var input = parseFloat($("#userInput").val());
     var falsey = falsey2(input);
     var sequence = sequenceFinal(input);
-    console.log(sequence);
-
 
     if (falsey === true){
       $(".display").show();
-      $(".falsey").show();
+      $(".falsey").toggle();
       $(".falsey").text("Ahh... now you're making it too hard, enter a whole number greater than 1!");
     } else if (falsey === false){
+      $("ul").show();
       $(".display").show();
-      $("#list").show();
       $(".falsey").hide();
       sequence.forEach (function(item) {
-        $("ul").append("<li>" + item + "<li>");
+        $("ul").append("<li>" + item + "</li>");
         });
     }
 
