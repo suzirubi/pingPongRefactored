bedb0d003d77dbc9b914d59758b903b5484f26c5
Suzi <Guest@epicodus-21.local>
2016-10-28 10:38:15 -0700

    update script file, html for sequence function

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,55 @@
 
 // business logic
 
-function falsey1(input) {
-  var falseString = input.toString();
-    if (falseString === "NaN") {
-      return true;
-    }
-}
 
 function falsey2(input) {
-  var falseString = input.toString();
-    if (falseString.match(/^[0-9]+$/) === null) {
-      return true;
-  }
-}
-
-function falsey3(input) {
-  var inputInt = parseFloat(input);
-  if ((inputInt % 1 != 0)|| inputInt <=1) {
+  if (input % 1 !== 0) {
     return true;
+  } else if (input % 1 === 0) {
+    return false;
   }
 }
 
-function sequence(input) {
-  var inputWhole = parseInt(input);
-  var sequenceArray = [];
+function sequenceFinal(input) {
+  var numberSequence = 
+  var sequenceArray = input.split(",");
 
-
-  for (var number = 1; number <= inputWhole; ++number) {
-  	sequenceArray.push(number);
-   }
-
-   console.log(sequence);
-
+  console.log(sequenceArray);
+  var output = sequenceArray;
+  return output;
 }
+
+
 
 
 // begin user interface
 $(document).ready(function() {
 
   $("#pingPongForm").submit(function(event) {
-    event.preventDefault();
 
-    var input = $("#userInput").val();
-    var false1 = falsey1(input);
-    var false2 = falsey2(input);
-    var false3 = falsey3(input);
-    var sequence1 = sequence(input);
+    $("ul").text("");
+
+    var input = parseFloat($("#userInput").val());
+    var falsey = falsey2(input);
+    var sequence = sequenceFinal(input);
+    console.log(sequence);
 
 
-    if (false1 = true) {
-      $(".falsey").toggle();
-      $(".falsey").text("Enter numbers silly!");
-    } else if (false2 = true) {
-      $(".falsey").toggle();
+    if (falsey === true){
+      $(".display").show();
+      $(".falsey").show();
       $(".falsey").text("Ahh... now you're making it too hard, enter a whole number greater than 1!");
-    } else if (false3 = true) {
-      $(".falsey").toggle();
-      $(".falsey").text("JUST numbers greater than 1");
+    } else if (falsey === false){
+      $(".display").show();
+      $("#list").show();
+      $(".falsey").hide();
+      sequence.forEach (function(item) {
+        $("ul").append("<li>" + item + "<li>");
+        });
     }
-    console.log(sequence1);
 
-      // groceryList.forEach(function(grocery){
-      //     $("ul").append("<li>" + grocery + "</li>");
-      // });
 
-      // $("#display").text(result);
-
+    event.preventDefault();
   });
 
 });
